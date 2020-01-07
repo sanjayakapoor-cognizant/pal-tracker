29c96047aaeb8b0651d21c5cab7dcfba0e94084f
Sanjaya <248453@cognizant.com>
2020-01-06 22:56:05 -0500

    gradle-files

--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -1,4 +1,21 @@
 package io.pivotal.pal.tracker;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class WelcomeController {
+
+    private String Msg;
+
+    @GetMapping("/")
+    public String sayHello() {
+        return this.Msg;
+    }
+    public WelcomeController(
+            @Value("${welcome.message}") String Message
+    ){
+        this.Msg = Message;
+    }
 }
